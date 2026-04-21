d7c7cc641c144d5bf1f20a05907e8122c7ccc530
Kateryna Ueno <katerynaueno@gmail.com>
2026-04-21 14:02:53 +0100

    Initial Commit

---
+++ Kat-Weather/Kat_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kat_WeatherApp.swift
+//  Kat-Weather
+//
+//  Created by Kateryna Ueno on 21/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Kat_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
